254b33fb54c33e63c8b2ff41bd35fad17cb2d8de
Adriano Gomes <adrianoso.sales@gmail.com>
2023-04-19 01:34:43 -0300

    Adicionado get para busca das tarefas e busca por ID

--- index.js
+++ index.js
@@ -6,3 +6,32 @@ app.use(express.json());
 app.listen(8080, () =>{
     console.log("Servidor ativo na porta 8080");
 })
+ let Tasks = [
+    {
+        id:1,
+        title: "Fazer API ",
+        description: "Desenvolver API ate dia 21.04",
+        completed: true
+      },
+      {
+        id:2,
+        title: "Estudar para a prova",
+        description: "Revisar os slides e livros",
+        completed: true
+      }
+ ]
+
+ //endpoint para listar as tarefas =)
+ app.get('/tasks', (req,res) =>{
+    res.send(Tasks)
+ })
+ app.get('/tasks/:id', (req, res) => {
+    const taskId = (req.params);
+    const task = Tasks.find(t => t.id === taskId);
+    if (task) {
+      res.send(task);
+    } else {
+      res.status(404).json({ message: 'Tarefa não encontrada' });
+    }
+  });
+ 
