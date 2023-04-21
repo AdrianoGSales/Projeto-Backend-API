9713bfe26f8fbd1c9ce77a38d1c6d0e5383010ff
Adriano Gomes <adrianoso.sales@gmail.com>
2023-04-21 16:40:37 -0300

    1.Incrementado função para gerar Id e realizar verificação.2. Realizado alterações no POST

--- index.js
+++ index.js
@@ -8,24 +8,42 @@ app.listen(8080, () =>{
 })
 let Tasks = [
 {
-  id:1,
+  id:"001",
   title: "Fazer API ",
   description: "Desenvolver API ate dia 21.04",
   completed: true
 },
 {
-  id:2,
+  id:"002",
   title: "Estudar para a prova",
   description: "Revisar os slides e livros",
   completed: true
 }]
+
+//GerarID
+let createId = () => {
+  let id = '';
+  for(i=id.length; i<3; i++) {
+    id += (Math.floor(Math.random()*9)).toString();
+  }
+  return id;
+}
+
+//VERIFICA ID PARA GERAR UM VALIDO E UNICO
+let checkId = () => {
+  let newId = createId();
+  while (Tasks.find((t)=>t.id ===newId)){
+    newId=checkId();
+  }
+  return newId;
+}
 
 //endpoint para listar as tarefas =)
 app.get('/tasks', (req,res) =>{
   res.send(Tasks)
 })
 
-
+//GET /tasks: Cria uma nova tarefa na lista.
 app.get('/tasks/:id', (req, res) => {
   const taskId = (req.params.id);
   const task = Tasks.find(t => t.id === taskId);
@@ -33,18 +51,25 @@ app.get('/tasks/:id', (req, res) => {
     res.send(task);
   }
   else {
-    res.json({ message: 'Tarefa não existe!!' });
+    res.status('404').json({ message: 'Tarefa não existe!!' });
   }
 });
 
 //POST /tasks: Cria uma nova tarefa na lista.
 app.post('/tasks', (req, res) => {
-  const task = req.body;
-  if (!task.id || !task.title) {
-    res.send('ID e Titulo é obrigatorio!!');
-  } else {
-    Tasks.push(task);
-    res.json(task);
+  let idExist = Tasks.find((t) => t.id === req.body.id);
+  if (!req.body.title) {
+    res.status(404).send('INFORME UM TITULO PARA A TAREFA!!!');
+    return
   }
+  newTask = {
+    id: checkId(),
+    title:req.body.title,
+    description: req.body.description,
+    completed: req.body.completed || false
+  }
+  Tasks.push(newTask);
+  res.send(`Tarefa numero ${newTask.id}:'${newTask.title}' criada`);
+  
 });
- 
+//PUT /tasks/:id 
